0d4aef282d7c8c423701b8e51b7e254a3307a99a
mamugo1432 <mamugo1432@gmail.com>
2025-12-12 09:13:02 +0100

    Añadiendo la creación del Contexto con la función que permite usar el contexto y el componente del mismo con el useState y un objeto que devuleve los datos que necesitarán los demás componentes

--- src/context/ShipContext.tsx
+++ src/context/ShipContext.tsx
@@ -1,1 +1,38 @@
-/*creatures context.tsx*/ 
+
+import type { spaceShift } from "../models/spaceShiftModels/interfaces";
+import type { RetournedValuesContext, ShipContentProps } from "./interfaces";
+import { useContext, createContext, useState } from "react";
+
+
+const ShipSpaceContext = createContext<RetournedValuesContext | null>(null);
+
+/**
+ * This function allow use the context if id doesn't have any error
+ * 
+ */
+export const useShip = () => {
+  const context = useContext(ShipSpaceContext);
+  if (!context) throw new Error('useShip debe usarse dentro de ShipContext');
+  return context;
+};
+
+/**
+ * This is the component that cover all the components that we would like to provide infomation
+ * @param {ShipContentProps} children 
+ * 
+ */
+export default function ShipContext({children}:ShipContentProps){
+
+    const [ship, setShip] = useState<spaceShift[]>([]);
+
+    const retournedValues : RetournedValuesContext = {
+        ship
+    }
+
+    return(
+        <ShipSpaceContext.Provider value={retournedValues}>
+            {children}
+        </ShipSpaceContext.Provider>
+    );
+
+}

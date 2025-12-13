905d9b1a10e7eb9e52054692b197a417a4cf7d55
Elias Molina <elias.m.v.2004@gmail.com>
2025-12-13 18:59:07 +0100

    Envolviendo la aplicacion con el shipProvider

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,23 @@ import Layout from "./components/layout";
 import CommandBridge from "./components/commandBridge";
 import Engage from "./components/engage";
 import Missions from "./components/missions";
+import ShipProvider from "./context/ShipContext";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Navigate to="/commandBridge" replace/>} />
-            <Route path="commandBridge" element={<CommandBridge />}></Route>
-            <Route path="engage" element={<Engage />}></Route>
-            <Route path="missions" element={<Missions />}></Route>
-            <Route path="*" element={ <h1> Error 404</h1>}></Route>
-          </Route>
-        </Routes>
+        <ShipProvider>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Navigate to="/commandBridge" replace/>} />
+              <Route path="commandBridge" element={<CommandBridge />}></Route>
+              <Route path="engage" element={<Engage />}></Route>
+              <Route path="missions" element={<Missions />}></Route>
+              <Route path="*" element={ <h1> Error 404</h1>}></Route>
+            </Route>
+          </Routes>
+        </ShipProvider>
       </BrowserRouter>
     </>
   )

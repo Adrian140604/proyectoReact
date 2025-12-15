df109f732e1741e14614002688a1ac3e9d78d2db
Elias Molina <elias.m.v.2004@gmail.com>
2025-12-15 09:02:55 +0100

    Subiendo componente de rutas separado

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,10 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import './App.css'
-import Layout from "./components/layout";
-import CommandBridge from "./components/commandBridge";
-import Engage from "./components/engage";
-import Missions from "./components/missions";
-import ShipProvider from "./context/ShipContext";
-import Error from "./components/Error";
+import Router from "./components/Router"
 
 const App = () => {
   return (
     <>
-      <BrowserRouter>
-        <ShipProvider>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<Navigate to="/commandBridge" replace/>} />
-              <Route path="commandBridge" element={<CommandBridge />}></Route>
-              <Route path="engage" element={<Engage />}></Route>
-              <Route path="missions" element={<Missions />}></Route>
-              <Route path="*" element={<Error/>}></Route>
-            </Route>
-          </Routes>
-        </ShipProvider>
-      </BrowserRouter>
+      <Router />
     </>
   )
 }

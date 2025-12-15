e59de5d29634eee4446361d3d2bbc714a104466b
Adrian140604 <adriansivianes88@gmail.com>
2025-12-15 09:26:55 +0100

    Exporto funciones de services

--- src/services/CharactersServices.ts
+++ src/services/CharactersServices.ts
@@ -1,11 +1,10 @@
 import type {Character} from "../models/charactersModels/interfaces"
 const url="https://rickandmortyapi.com/api/character";
 
-const getCharacters=async (): Promise<Character[]>=>{
+export const getCharacters=async (): Promise<Character[]>=>{
     let response= await fetch(url);
     let responseJson=await response.json();
     return responseJson.results as Character[];
     
 }
 
-getCharacters();
--- src/services/LocationsServices.ts
+++ src/services/LocationsServices.ts
@@ -2,10 +2,9 @@ import type {Places} from "../models/locationsModels/interfaces"
 const url="https://rickandmortyapi.com/api/location";
 
 
-const getPlaces = async ():Promise <Places []> =>{
+export const getPlaces = async ():Promise <Places []> =>{
     let response= await fetch(url);
     let responseJson=await response.json();
     return responseJson.result as Places[]
 }
 
-getPlaces();

eefc2c2e779dca565d28d408126ac1be34975156
Elias Molina <elias.m.v.2004@gmail.com>
2025-12-17 13:13:40 +0100

    Error estilo

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,23 +1,17 @@
 const Error = () => {
   return (
-    <>
-        <h1> Error 404</h1>
-        <span className="badge d-inline-flex align-items-center gap-2 py-1 px-2 rounded-pill bg-success-subtle text-success border border-success-subtle fw-medium">
-        <span className="d-inline-flex align-items-center">
-            <i className="bi bi-currency-euro"></i>
-        </span>
-        <span className="text-nowrap">Dinero</span>
-        <span className="badge bg-transparent border-0 p-0 m-0 text-nowrap">15.200 €</span>
-        </span>
-
-        <span className="badge d-inline-flex align-items-center gap-2 py-1 px-2 rounded-pill bg-warning-subtle text-warning border border-warning-subtle fw-medium">
-        <span className="d-inline-flex align-items-center">
-            <i className="bi bi-fuel-pump"></i>
-        </span>
-        <span className="text-nowrap">Combustible</span>
-        <span className="badge bg-transparent border-0 p-0 m-0 text-nowrap">75%</span>
-        </span>
-
+    <> 
+    <div className="container d-flex justify-content-center align-items-center vh-100">
+      <div className="card shadow-lg p-5 text-center">
+        <h1 className="display-1 text-danger fw-bold">¡ERROR!</h1>
+        <p className="fs-4 text-muted">
+          Algo salió mal. Intenta nuevamente más tarde.
+        </p>
+        <a href="/" className="btn btn-danger btn-lg mt-3">
+          Volver al inicio
+        </a>
+      </div>
+    </div>
     </>
   );
 };
